4fe4984d57c382b73b62d1a4a6581de1299624b3
ForAppDeveloper <51786902+ForAppDeveloper@users.noreply.github.com>
2022-05-17 14:53:06 +0800

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,11 @@
-
-// << Determine Login or Register page >>
 function checkTxtEmailExist(){
     try{ txtEmail }catch(e){console.log(e);}
 }
 
 function checkUsernameExist(){
-    try{ txtEmail }catch(e){console.log(e);}
+    try{ username }catch(e){console.log(e);}
 }
 
-// << Auto Login >>
 function  fillUsername(value){
     username.value = value;
 }
@@ -21,7 +18,6 @@ function  clickSubmit(){
     btnSubmit.click();
 }
 
-// << Check Error >>
 function clearStorageError(){
     try { sessionStorage.setItem('error', 0); } catch(e){ console.log(e); }
 }
@@ -31,10 +27,9 @@ function constantlyCheckError(){
 }
 
 function getError(){
-    sessionStorage.getItem('error');
+    return sessionStorage.getItem('error');
 }
 
-// << Login >>
 function removeLoginStorageItem(value){
     sessionStorage.removeItem(value);
 }
@@ -52,28 +47,25 @@ function listenSubmitOnclick(){
 }
 
 function getLoginStorageItem(value){
-   return sessionStorage.getItem(value);
+    return sessionStorage.getItem(value);
 }
-
-// << Register >>
 
 function removeRegStorageItem(value){
     sessionStorage.removeItem(value);
 }
 
-function listemTxtEmail(value){
+function listenTxtEmail(value){
     try{ txtEmail.oninput = function() {var biometric = sessionStorage.getItem(value);if(biometric && biometric != null){ biometric = JSON.parse(biometric); biometric.username = txtEmail.value; biometric = JSON.stringify(biometric);}else{biometric = {username: txtEmail.value,password: null}; biometric = JSON.stringify(biometric);}sessionStorage.setItem(value, biometric);}; } catch(e){ console.log(e)};
 }
 
-function listemTxtPassword(value){
+function listenTxtPassword(value){
     try{  if(txtPassword.valueOf()[0]){passwordField = txtPassword.valueOf()[0] ;}else{ passwordField = txtPassword; }; passwordField.oninput = function() {var biometric = sessionStorage.getItem(value);if(biometric && biometric != null){ biometric = JSON.parse(biometric); biometric.password = passwordField.value; biometric = JSON.stringify(biometric);}else{biometric = {username: null,password: passwordField.value}; biometric = JSON.stringify(biometric);}sessionStorage.setItem(value, biometric);}; } catch(e){ console.log(e)};
 }
 
 function getRegisterStorageItem(value){
-    sessionStorage.getItem(value);
+    return sessionStorage.getItem(value);
 }
 
-// << SSL Error alert >>
 function alertSSLError(){
     alert('An SSL error has occured and a secure connection to the server cannot be made.');
 }
